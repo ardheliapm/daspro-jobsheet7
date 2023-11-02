bf06e799849c025c0e2c684ed192480550c0d560
dedelduwell <ardheliamputri@gmail.com>
2023-11-02 13:53:48 +0700

    Modifikasi While

--- ForKelipatan05.java
+++ ForKelipatan05.java
@@ -9,22 +9,43 @@ public class ForKelipatan05 {
         int kelipatan;
         int jumlah = 0;
         int counter = 0;
+        double rata = 0;
         Scanner scan  = new Scanner(System.in);
         System.out.println("Masukkan bilangan kelipatan (1-9) : ");
         kelipatan = scan.nextInt();
 
        
-		for (int i = 1 ; i<= 50 ; i++) {
+		/*for (int i = 1 ; i<= 50 ; i++) {
             if (i % kelipatan == 0) {
                 jumlah += i;
                 counter++;
-            }
+            }rata = (double) jumlah / counter;
         }
+        /* */
+
+        int i = 1;
+        while (i <= 50) 
+        {
+           if (i % kelipatan == 0) 
+           {
+            jumlah += i;
+            counter++;
+
+         
+           }
+             i++; 
+            //rata = (double) jumlah / counter; 
+        }
+        
+        
+
+         rata = (double) jumlah / counter; 
         System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan,counter);
    
 		System.out.printf("Total Bilangan kelipatan %d dari 1 sampai 50 adalah %d\n" , kelipatan, jumlah);
         
 
+System.out.println("Rata- Rata dari seluruh Bilangan adalah : "+ rata);
 
     }
 }
