078b94592db2fa97cc1adc81bca6d4118bfdfa3c
dedelduwell <ardheliamputri@gmail.com>
2023-11-03 11:53:02 +0700

    Modifikasi 1

---
+++ DoWhileCuti05.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+/**
+ * DoWhileCuti05
+ */
+public class DoWhileCuti05 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int jatahCuti,jumlahHari;
+        String konfirmasi;
+
+        System.out.println("Jatah Cuti : ");
+        jatahCuti = sc.nextInt();
+
+        do{
+            System.out.print("Apakah Anda ingin mengambil cuti  (y/t) ? : ");
+            konfirmasi = sc.next();
+                if (konfirmasi.equalsIgnoreCase("y")) {
+                    System.out.println("Jumlah Hari : ");
+                    jumlahHari= sc.nextInt();
+
+                        if (jumlahHari <= jatahCuti) {
+                            jatahCuti -= jumlahHari;
+                            System.out.println("Sisa jatah cuti : " + jatahCuti);
+                        } else {
+                            System.out.println("Sisa jatah cuti Anda tidak mencukupi");
+                            System.out.println("Maaf Salah, Silahkan Masukkan Jatah Cuti Anda Lagi: ");
+                            konfirmasi = sc.next();
+
+                            if (konfirmasi.equalsIgnoreCase("y")) {
+                                continue;
+                            } else {
+                                System.out.println("Terimakasih");
+                                break;
+                            }
+                            
+                           
+                        }
+
+                    }
+        } while (jatahCuti > 0);
+
+    }
+}
